9a5a46992eafc427c49e076f0e1cd4c57171f8be
Sean Abraham <seanabraham@gmail.com>
2019-03-02 20:07:20 -0700

    Get the Wednesday reminder code to send an email at the end

--- cmd/wednesday-reminder/wednesday-reminder.go
+++ cmd/wednesday-reminder/wednesday-reminder.go
@@ -14,19 +14,22 @@ import (
 )
 
 var (
-	futureRosterURL = flag.String("future-roster-url", "http://www.parkrun.us/southbouldercreek/futureroster/", "URL for a parkrun future roster page")
+	futureRosterURL     = flag.String("future-roster-url", "http://www.parkrun.us/southbouldercreek/futureroster/", "URL for a parkrun future roster page")
+	necessaryVolunteers = flag.String("necessary-volunteers", "Run Director,Equipment Storage and Delivery,Timekeeper,Barcode Scanning,Finish Tokens,Marshal,Marshal", "Comma-separated list of required volunteer positions, possibly including duplicates")
 )
 
 func main() {
 	flag.Parse()
 
+	// TODO: we don't need all these scopes, but I need to make this binary
+	// store a differently-named oauth token to avoid needing to manually re-auth.
 	if err := googleapis.AddScope(gmail.GmailSendScope,
 		photoslibrary.PhotoslibraryAppendonlyScope,
 		photoslibrary.PhotoslibraryReadonlyScope,
 		photoslibrary.PhotoslibrarySharingScope); err != nil {
 		log.Fatal(err)
 	}
-	_, err := googleapis.GetClient()
+	googleClient, err := googleapis.GetClient()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,14 +42,48 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	necessaryVolunteersCounts := make(map[string]int)
+	for _, v := range strings.Split(*necessaryVolunteers, ",") {
+		necessaryVolunteersCounts[v]++
+	}
+	for _, rv := range nextEvent.RoleVolunteers {
+		if necessaryVolunteersCounts[rv.Role] > 0 {
+			necessaryVolunteersCounts[rv.Role]--
+		}
+	}
+	missingVolunteersStrs := make([]string, 0)
+	for k, v := range necessaryVolunteersCounts {
+		if v > 0 {
+			missingVolunteersStrs = append(missingVolunteersStrs, fmt.Sprintf("%d %s", v, k))
+		}
+	}
+	missingVolunteersMsg := strings.Join(missingVolunteersStrs, ", ")
+
 	nextRd := nextEvent.VolunteersForRole("Run Director")
 	dateStr := nextEvent.Date.Format("2006-01-02")
 	nextRdStr := strings.Trim(fmt.Sprint(nextRd), "[]")
-	subject := "Update for run on " + dateStr
-	message := fmt.Sprintf(
-		"Hi run director %s,\n"+
-			"Here is the roster for the upcoming run:\n"+
-			"%v", nextRdStr, nextEvent)
-	log.Printf("Would use subject\n%s", subject)
-	log.Printf("Would send message\n%s", message)
+
+	subject := ""
+	if len(missingVolunteersMsg) > 0 {
+		subject = fmt.Sprintf("[Automated] Volunteers needed for %v event", dateStr)
+	} else {
+		subject = fmt.Sprintf("[Automated] No additional volunteers needed for %v event", dateStr)
+	}
+
+	message := fmt.Sprintf("Hi run director %s,\n\n", nextRdStr)
+	if len(missingVolunteersMsg) > 0 {
+		message += fmt.Sprintf("We still need volunteers for these roles: %s\n\n", missingVolunteersMsg)
+	} else {
+		message += "We have all the required roles filled for the next run!\n\n"
+	}
+	message += fmt.Sprintf("Here is the roster as of now:\n%v\n\n", nextEvent)
+	message += fmt.Sprintf("You can see the roster on the web at %s", *futureRosterURL)
+
+	log.Printf("Email subject\n%s", subject)
+	log.Printf("Email message\n%s", message)
+
+	if err = googleapis.SendEmail(googleClient, "me", "me", "seanabraham@gmail.com", subject, message); err != nil {
+		log.Fatal(err)
+	}
 }
